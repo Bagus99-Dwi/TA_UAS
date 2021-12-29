9ce0e5d37886dd8e020884dedda2b4e2930816fb
Bagus99-Dwi <72676968+Bagus99-Dwi@users.noreply.github.com>
2021-12-29 17:15:10 +0700

    Add files via upload

---
+++ retrofit2listview/app/src/main/java/com/bagusdwip/retrofit2listview/rest/RestAPI.java
@@ -0,0 +1,14 @@
+package com.bagusdwip.retrofit2listview.rest;
+import com.bagusdwip.retrofit2listview.models.Model;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by Teknorial on 10/02/2016.
+ */
+public interface RestAPI{
+
+    @GET("example/buku")
+    Call<Model> loadListBook();
+}
